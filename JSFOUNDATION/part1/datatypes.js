d9cb38ed23e7ccee86194170c62cec05e9b72db2
ansh pandey <anshpndey@gmail.com>
2025-05-01 19:46:25 +0530

    update

---
+++ JSFOUNDATION/part1/datatypes.js
@@ -0,0 +1,32 @@
+console.log("hello");
+// console.log("chai");
+/*
+
+String 
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102
+
+let score = 102;
+let name = "chaicode.com";
+let isLoggedin = false;
+
+//object
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = { firstname: "ansh", lastname: "pandey" };
+
+let getScore = score;
+
+console.log(score);
+console.log(getScore);
---
+++ JSFOUNDATION/part1/printing.js
@@ -0,0 +1,5 @@
+console.log("hello");
+console.log("hello");
+process.stdout.write("chai");
+process.stdout.write("chai");
+console.table({ city: "jaipur" });
