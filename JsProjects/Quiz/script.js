922b62be1def5d43f973b97144c61b253c9a4556
ansh pandey <anshpndey@gmail.com>
2025-05-15 01:38:37 +0530

    update

---
+++ JsProjects/Quiz/script.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startBtn = document.getElementById("start-btn");
+  const nextBtn = document.getElementById("next-btn");
+  const restartBtn = document.getElementById("restart-btn");
+  const questionContainer = document.getElementById("question-container");
+  const questionText = document.getElementById("question-next");
+  const choicesList = document.getElementById("choices-list");
+  const resultContainer = document.getElementById("result-container");
+  const scoreDisplay = document.getElementById("score");
+
+  const questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      choices: ["Mars", "Venus", "Jupiter", "saturn"],
+      answer: "Mars",
+    },
+    {
+      question: "Who wrote Hmalet?",
+      choices: [
+        "Charles Dickens",
+        "jane Austen",
+        "William Shakespeare",
+        "Mark Twain",
+      ],
+      answer: "William Shakespeare",
+    },
+  ];
+
+  let currentQuestionIndex = 0;
+  let score = 0;
+
+  startBtn.addEventListener("click", startQuiz);
+
+  nextBtn.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      showQuestion();
+    } else {
+      showResult();
+    }
+  });
+
+  restartBtn.addEventListener("click", () => {
+    currentQuestionIndex = 0;
+    score = 0;
+    resultContainer.classList.add("hidden");
+    startQuiz();
+  });
+
+  function startQuiz() {
+    //parameter not passed so no callback function required
+    startBtn.classList.add("hidden");
+    questionContainer.classList.remove("hidden");
+    resultContainer.classList.add("hidden");
+    showQuestion();
+  }
+
+  function showQuestion() {
+    nextBtn.classList.add("hidden");
+    questionText.textContent = questions[currentQuestionIndex].question;
+    choicesList.innerHTML = ""; //clear previous choices
+    questions[currentQuestionIndex].choices.forEach((choice) => {
+      const li = document.createElement("li");
+      li.textContent = choice;
+      li.addEventListener("click", () => selectAnswer(choice)); //parameter passed so callback function required
+      choicesList.appendChild(li);
+    });
+  }
+
+  function selectAnswer(choice) {
+    const correctAnswer = questions[currentQuestionIndex].answer;
+    if (choice === correctAnswer) {
+      score++;
+    }
+    nextBtn.classList.remove("hidden");
+  }
+
+  function showResult() {
+    questionContainer.classList.add("hidden");
+    resultContainer.classList.remove("hidden");
+    scoreDisplay.textContent = `${score} out of ${questions.length}`;
+  }
+});
