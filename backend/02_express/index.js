99fc8c8220bf6662985620c39c40fdd2a5808909
ansh pandey <anshpndey@gmail.com>
2025-05-24 16:28:04 +0530

    update

--- backend/02_express/index.js
+++ backend/02_express/index.js
@@ -1,15 +1,37 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
 
 //add a new tea
 app.post("/teas", (req, res) => {
+  logger.info("a post request for new tea is added");
+  logger.warn("a post request for new tea is added");
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
   teaData.push(newTea);
