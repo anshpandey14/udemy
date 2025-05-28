b5616feb901746e9a3c180ca48e0a05b8c2aa528
ansh pandey <anshpndey@gmail.com>
2025-05-28 22:53:25 +0530

    add tweet controller

--- backend/VIDTUBE/src/controllers/tweet.controllers.js
+++ backend/VIDTUBE/src/controllers/tweet.controllers.js
@@ -45,4 +45,70 @@ const getUserTweets = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, tweets, "Users Tweets fetched successfully"));
 });
 
-export { createTweet, getUserTweets };
+const updateTweet = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const { content } = req.body;
+  const tweetId = req.params.id;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user Id");
+  }
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid Tweet Id");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(400, "Tweet not found");
+  }
+
+  if (tweet.owner.toString() !== userId.toString()) {
+    throw new ApiError(400, "You are not authorized to update the tweet");
+  }
+
+  if (content !== undefined) {
+    tweet.content = content;
+  }
+  await tweet.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet updated successfully"));
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const tweetId = req.params.id;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user Id");
+  }
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid Tweet Id");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(400, "Tweet not found");
+  }
+
+  if (tweet.owner.toString() !== userId.toString()) {
+    throw new ApiError(403, "You are not authorized to delete this tweet");
+  }
+
+  await tweet.deleteOne();
+
+  await User.findByIdAndUpdate(userId, {
+    $pull: { tweets: tweetId },
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, {}, "Tweet deleted successfully"));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
