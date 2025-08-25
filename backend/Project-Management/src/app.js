54d8ded1fd74266de2af27925f557b1439a23d64
ansh pandey <anshpndey@gmail.com>
2025-08-26 01:02:02 +0530

    update

---
+++ backend/Project-Management/src/app.js
@@ -0,0 +1,33 @@
+//express configurations and setup file
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// basic configurations of express
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+// import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Project-Management");
+});
+
+export default app;
