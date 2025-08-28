680ac9e057ef0a2eadf999d6b233bd0a51761954
ansh pandey <anshpndey@gmail.com>
2025-08-28 22:24:59 +0530

    update

---
+++ backend/Project-Management/src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be in lowercase")
+      .isLength({ min: 3 })
+      .withMessage("username must be atleast 3 characters"),
+    body("password").trim().notEmpty().withMessage("Password is rerquired"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
