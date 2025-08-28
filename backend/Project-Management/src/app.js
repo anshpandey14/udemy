c762cb30aeb8102e587ff89b2f207981149f7351
ansh pandey <anshpndey@gmail.com>
2025-08-28 23:43:24 +0530

    update

--- backend/Project-Management/src/app.js
+++ backend/Project-Management/src/app.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use(cookieParser());
 
 // cors configurations
 app.use(
@@ -24,6 +27,7 @@ app.use(
 
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import { cookie } from "express-validator";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
