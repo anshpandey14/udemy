5b5d2a9a62755095c2f544472a4db20b06d0aece
ansh pandey <anshpndey@gmail.com>
2025-09-09 21:08:43 +0530

    update

--- react-course/01basicvite/src/main.jsx
+++ react-course/01basicvite/src/main.jsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
 createRoot(document.getElementById("root")).render(<App />);
