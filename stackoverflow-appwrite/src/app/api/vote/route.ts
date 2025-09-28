15b0d74de71206d21f962b15006ba6a96afab102
anshpandey14 <anshpndey@gmail.com>
2025-09-28 20:19:36 +0530

    update

--- stackoverflow-appwrite/src/app/api/vote/route.ts
+++ stackoverflow-appwrite/src/app/api/vote/route.ts
@@ -1,7 +1,13 @@
-import { db, voteCollection } from "@/models/name";
-import { databases } from "@/models/server/config";
+import {
+  answerCollection,
+  db,
+  questionCollection,
+  voteCollection,
+} from "@/models/name";
+import { databases, users } from "@/models/server/config";
+import { UserPrefs } from "@/store/auth";
 import { NextRequest, NextResponse } from "next/server";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
 export async function POST(request: NextRequest) {
   try {
@@ -16,11 +22,73 @@ export async function POST(request: NextRequest) {
     ]);
 
     if (response.documents.length > 0) {
-      //
+      await databases.deleteDocument(
+        db,
+        voteCollection,
+        response.documents[0].$id
+      );
+
+      //decrease the reputation of the question/answer author
+      const QuestionOrAnswer = await databases.getDocument(
+        db,
+        type == "question" ? questionCollection : answerCollection,
+        typeId
+      );
+
+      const authorPrefs = await users.getPrefs<UserPrefs>(
+        QuestionOrAnswer.authorId
+      );
+
+      await users.updatePrefs<UserPrefs>(QuestionOrAnswer.authorId, {
+        reputation:
+          response.documents[0].voteStatus === "upvoted"
+            ? Number(authorPrefs.reputation) - 1
+            : Number(authorPrefs.reputation) + 1,
+      });
     }
 
     //that means previous vote doesnt exist or vote status changes
     if (response.documents[0]?.voteStatus !== voteStatus) {
+      const doc = await databases.createDocument(
+        db,
+        voteCollection,
+        ID.unique(),
+        {
+          type,
+          typeId,
+          voteStatus,
+          votedById,
+        }
+      );
+
+      //increase or decrease the reputation of the question/answer author accordingly
+      const QuestionOrAnswer = await databases.getDocument(
+        db,
+        type === "question" ? questionCollection : answerCollection,
+        typeId
+      );
+      const authorPrefs = await users.getPrefs<UserPrefs>(
+        QuestionOrAnswer.authorId
+      );
+
+      // if vote was present
+      if (response.documents[0]) {
+        await users.updatePrefs<UserPrefs>(QuestionOrAnswer.authorId, {
+          reputation:
+            //that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+            response.documents[0].voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) - 1
+              : Number(authorPrefs.reputation) + 1,
+        });
+      } else {
+        await users.updatePrefs<UserPrefs>(QuestionOrAnswer.authorId, {
+          reputation:
+            //that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+            voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) + 1
+              : Number(authorPrefs.reputation) - 1,
+        });
+      }
     }
 
     const [upvotes, downvotes] = await Promise.all([
@@ -29,16 +97,29 @@ export async function POST(request: NextRequest) {
         Query.equal("typeId", typeId),
         Query.equal("voteStatus", "upvoted"),
         Query.equal("votedById", votedById),
-        Query.limit(1)
+        Query.limit(1),
       ]),
       databases.listDocuments(db, voteCollection, [
         Query.equal("type", type),
         Query.equal("typeId", typeId),
         Query.equal("voteStatus", "downvoted"),
         Query.equal("votedById", votedById),
-        Query.limit(1)
+        Query.limit(1),
       ]),
     ]);
+
+    return NextResponse.json(
+      {
+        data: {
+          document: null,
+          voteResult: upvotes.total == downvotes.total,
+        },
+        message: "vote handled",
+      },
+      {
+        status: 200,
+      }
+    );
   } catch (error: any) {
     return NextResponse.json(
       {
