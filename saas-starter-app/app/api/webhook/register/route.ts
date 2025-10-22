7727faf30305dbbbc10af85f8c29b78c1c3a22bc
anshpandey14 <anshpndey@gmail.com>
2025-10-23 02:44:03 +0530

    add subscription route

---
+++ saas-starter-app/app/api/webhook/register/route.ts
@@ -0,0 +1,66 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  if (!WEBHOOK_SECRET) {
+    throw new Error("Please add WEBHOOK_SECRET to your .env");
+  }
+
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Missing Svix headers", { status: 400 });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+  const wh = new Webhook(WEBHOOK_SECRET);
+  let evt: WebhookEvent;
+
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Invalid webhook signature", { status: 400 });
+  }
+
+  const { id } = evt.data;
+  const eventType = evt.type;
+
+  console.log(`✅ Webhook received: ${eventType} (${id})`);
+
+  if (evt.type === "user.created" || evt.type === "user.updated") {
+    const { email_addresses, primary_email_address_id } = evt.data;
+
+    const primaryEmail = email_addresses?.find(
+      (email) => email.id === primary_email_address_id
+    );
+
+    if (!primaryEmail) {
+      console.log("No primary email yet, skip for now");
+      return new Response("No primary email yet", { status: 200 });
+    }
+
+    await prisma.user.create({
+      data: {
+        id: evt.data.id!,
+        email: primaryEmail.email_address,
+        isSubscribed: false,
+      },
+    });
+    console.log(`✅ User created: ${primaryEmail.email_address}`);
+  }
+
+
+  return new Response("Webhook processed successfully", { status: 200 });
+}
