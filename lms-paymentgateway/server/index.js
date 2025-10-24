2987b493ae67b97655900035b3fe8ea6698ceb7a
anshpandey14 <anshpndey@gmail.com>
2025-10-24 20:17:33 +0530

    add basic setup and security

---
+++ lms-paymentgateway/server/index.js
@@ -0,0 +1,90 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+dotenv.config();
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT;
+
+//Global rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 Minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too manyrequests from this IP, please try later",
+});
+
+//security middleware
+app.use(helmet());
+app.use(ExpressMongoSanitize());
+app.use(hpp());
+app.use("/api", limiter);
+
+//logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Body Parser Middleware
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10kb",
+  })
+);
+app.use(cookieParser());
+
+//GLobal Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    status: "error",
+    message: err.message || "Internal server error",
+    ...(process.env.NODE_ENV === "development" && {
+      stack: err.stack,
+    }),
+  });
+});
+
+//CORS configuration
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
+  })
+);
+
+//API Routes
+
+//it should be always at bottom
+//404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found !!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode`);
+});
